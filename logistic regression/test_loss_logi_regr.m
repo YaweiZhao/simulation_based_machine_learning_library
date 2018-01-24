fa2aafcbfa34d984e3ba5a49f0fee926217eb36b
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-01-24 09:10:10 -0500

    d
    d

--- logistic regression/test_loss_logi_regr.m
+++ logistic regression/test_loss_logi_regr.m
@@ -11,7 +11,7 @@ training_data = data(:,2:d);
 training_data = [training_data ones(n,1)];% add 1-offset
 [n,d] = size(training_data);
 %initialize parameters
-eta = 1e-1;%learning rate
+eta = 1e-3;%learning rate
 gamma = 1e-3;% regularization coefficient
 T = 100;%total number of iterations
 x = zeros(d,1);%the initial parameter
@@ -25,9 +25,15 @@ Q = Q + eye(d);
 
 loss_init = 1/n*sum(log(1+exp(-1*label .* (training_data*x))));
 
+b = fix(0.01*n);%mini-batch
 for t=1:T
-    i = randi(n);
-    nabla_x = -(transpose(training_data(i,:))*label(i,:))/(1+exp(label(i,:)*training_data(i,:)*x));
+    stoc_nabla_x = zeros(d,1);
+    for j=1:b
+        i = randi(n);
+        stoc_nabla_x_temp = -(transpose(training_data(i,:))*label(i,:))/(1+exp(label(i,:)*training_data(i,:)*x));
+        stoc_nabla_x = stoc_nabla_x + stoc_nabla_x_temp;
+    end
+    stoc_nabla_x = 1/b*stoc_nabla_x;
     cvx_begin
         variable x_unknown(d,1)
         temp1 = norm(Q*(x_unknown-x),1);
