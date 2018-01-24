b91d72b680520188f040f072f823da4db50df149
Yawei Zhao <zhaoyawei@nudt.edu.cn>
2018-01-24 11:11:40 -0500

    d
    d

--- logistic regression/result_visualization.m
+++ logistic regression/result_visualization.m
@@ -1,15 +1,21 @@
 clear;
+%% logistic regression
 %plot loss
-% loss = load('loss.mat');
-% loss_data = loss.loss;
-% num_iter = 1:100;%number of iterations
-% plot(num_iter,loss_data);
-% xlabel('# of iterations');
-% ylabel('loss');
+loss = load('loss.mat');
+loss_data = loss.loss;
+num_iter = 1:100;%number of iterations
+plot(num_iter,loss_data);
+xlabel('# of iterations');
+ylabel('loss');
 %hold on;
+
+
 %plot the change of x
-delta_x = load('delta_x');
-delta_x_data = delta_x.delta_x;
-plot(delta_x_data);
-xlabel('dimensions');
-ylabel('change of x');
+% delta_x = load('delta_x');
+% delta_x_data = delta_x.delta_x;
+% plot(delta_x_data);
+% xlabel('dimensions');
+% ylabel('change of x');
+
+
+%% ridge regression
